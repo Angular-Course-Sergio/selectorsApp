5469136a24d686c6d00bc0dbd34fa2733e52eb97
Sergio Barreras <sergiobg.isc@gmail.com>
2024-04-21 15:55:29 -0700

    show countries on selector

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Region } from '../interfaces/country.interfaces';
+import {
+  Country,
+  Region,
+  SmallCountry,
+} from '../interfaces/country.interfaces';
+import { Observable, map, of, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountriesService {
-  constructor() {}
+  private baseUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor(private http: HttpClient) {}
 
   private _regions: Region[] = [
     Region.Africa,
@@ -17,5 +25,19 @@ export class CountriesService {
 
   get regions(): Region[] {
     return [...this._regions];
+  }
+
+  getCountriesByRegion(region: Region): Observable<SmallCountry[]> {
+    if (!region) return of([]);
+    const url = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+    return this.http.get<Country[]>(url).pipe(
+      map((countries) =>
+        countries.map((country) => ({
+          name: country.name.common,
+          cca3: country.cca3,
+          borders: country.borders ?? [],
+        }))
+      )
+    );
   }
 }
